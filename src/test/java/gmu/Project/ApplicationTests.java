0a700f686a20e76b3ed2ed3fe1fdf9787da52b8a
ryan-hoang <rhoang@gmu.edu>
2021-04-02 17:54:36 -0400

    Initial Commit

---
+++ src/test/java/gmu/Project/ApplicationTests.java
@@ -0,0 +1,13 @@
+package gmu.Project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
