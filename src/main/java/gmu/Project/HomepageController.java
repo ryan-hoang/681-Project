929e5c4c3b2c064b08185643a362a0351e49074a
ajcordani <ajcordani@gmail.com>
2021-04-06 16:16:01 -0400

    Added dependencies, mysql config, and temp homepage

---
+++ src/main/java/gmu/Project/HomepageController.java
@@ -0,0 +1,14 @@
+package gmu.Project;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class HomepageController {
+
+    @RequestMapping(value = "/", method=RequestMethod.GET)
+    public String rootView () {
+        return "index";
+    }
+}
