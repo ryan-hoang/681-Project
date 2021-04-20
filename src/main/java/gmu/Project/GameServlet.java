cc8946dc502215030fca9a0a4ad26c37c0cbbf56
Ryan Hoang <rhoang@gmu.edu>
2021-04-19 20:59:35 -0400

    Delete Homepage.html, start GameServlet.java

--- src/main/java/gmu/Project/GameServlet.java
+++ src/main/java/gmu/Project/GameServlet.java
@@ -1,4 +1,16 @@
 package gmu.Project;
 
-public class GameServlet {
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class GameServlet extends HttpServlet
+{
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
+    {
+
+    }
 }
