e559e2b27827d9dfedf531c2038f07d9dee96f8a
Ryan Hoang <rhoang@gmu.edu>
2021-04-20 18:25:10 -0400

    Test Register Servlet

--- src/main/java/gmu/Project/Application.java
+++ src/main/java/gmu/Project/Application.java
@@ -2,12 +2,26 @@ package gmu.Project;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
 
-@SpringBootApplication
+import java.util.Arrays;
+
+@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
 public class Application {
 
-	public static void main(String[] args) {
+	public static void main(String[] args)
+	{
 		SpringApplication.run(Application.class, args);
+	}
+
+	@Bean
+	ServletRegistrationBean myServletRegistration () {
+		ServletRegistrationBean srb = new ServletRegistrationBean();
+		srb.setServlet(new GameServlet());
+		srb.setUrlMappings(Arrays.asList("/game"));
+		return srb;
 	}
 
 }
--- src/main/java/gmu/Project/GameServlet.java
+++ src/main/java/gmu/Project/GameServlet.java
@@ -5,12 +5,22 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 public class GameServlet extends HttpServlet
 {
     @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
     {
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
 
+        // send HTML page to client
+        out.println("<html>");
+        out.println("<head><title>A Test Servlet</title></head>");
+        out.println("<body>");
+        out.println("<h1>Test</h1>");
+        out.println("<p>Simple servlet for testing.</p>");
+        out.println("</body></html>");
     }
 }
