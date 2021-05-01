ef9a512f08083dbdabcf9f0836d756106e491b25
ajcordani <ajcordani@gmail.com>
2021-04-30 23:33:52 -0400

    test

---
+++ src/test/java/gmu/Project/HandEvaluatorTest.java
@@ -0,0 +1,44 @@
+package gmu.Project;
+
+import org.junit.jupiter.api.Test;
+
+import static gmu.Project.HandComparison.compareHands;
+import static gmu.Project.HandComparison.determineHand;
+import static gmu.Project.HandEvaluator.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+
+class HandEvaluatorTest {
+
+    @Test
+    public void cardTester(){
+        Card[] hand = new Card[5];
+        Card a = new Card(Card.CardValue.JACK, Card.Suit.CLUBS);
+        Card b = new Card(Card.CardValue.JACK, Card.Suit.CLUBS);
+        Card c = new Card(Card.CardValue.QUEEN, Card.Suit.DIAMONDS);
+        Card d = new Card(Card.CardValue.EIGHT, Card.Suit.CLUBS);
+        Card e = new Card(Card.CardValue.TEN, Card.Suit.CLUBS);
+        hand[0] = a;
+        hand[1] = b;
+        hand[2] = c;
+        hand[3] = d;
+        hand[4] = e;
+        Card[] hand2 = new Card[5];
+        Card f = new Card(Card.CardValue.QUEEN, Card.Suit.DIAMONDS);
+        Card g = new Card(Card.CardValue.QUEEN, Card.Suit.HEARTS);
+        Card h = new Card(Card.CardValue.QUEEN, Card.Suit.HEARTS);
+        Card i = new Card(Card.CardValue.NINE, Card.Suit.HEARTS);
+        Card j = new Card(Card.CardValue.TEN, Card.Suit.HEARTS);
+        hand2[0] = f;
+        hand2[1] = g;
+        hand2[2] = h;
+        hand2[3] = i;
+        hand2[4] = j;
+        String result = compareHands(hand, hand2);
+        System.out.println(result);
+
+        return;
+    }
+
+
+}
